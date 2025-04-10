db1001b9afeeff4759d5953acc0ef381462dce2a
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-04-11 01:19:41 +0900

    fix: Config 파일 경로 변경

--- src/main/java/org/example/omnibeuser/common/config/CorsConfig.java
+++ src/main/java/org/example/omnibeuser/common/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package org.example.omnibeuser.config;
+package org.example.omnibeuser.common.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
