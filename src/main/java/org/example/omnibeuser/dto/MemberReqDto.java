94e03780a9f23636389aea5ed2e23f16bbe33741
Habeomsu <hbs5186@naver.com>
2025-04-17 10:59:47 +0900

    #27 유효성 처리 구현

--- src/main/java/org/example/omnibeuser/dto/MemberReqDto.java
+++ src/main/java/org/example/omnibeuser/dto/MemberReqDto.java
@@ -1,5 +1,8 @@
 package org.example.omnibeuser.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 public class MemberReqDto {
@@ -7,47 +10,78 @@ public class MemberReqDto {
     @Getter
     public static class NormalSignup{
 
-        String name;
-        String loginId;
-        String password;
-        String eqPassword;
-        String cardPassword;
+        @NotBlank(message = "이름은 필수입니다.")
+        private String name;
+
+        @NotBlank(message = "아이디는 필수입니다.")
+        private String loginId;
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
+
+        @NotBlank(message = "확인 비밀번호는 필수입니다.")
+        private String eqPassword;
+
+        @NotBlank(message = "카드 비밀번호는 필수입니다.")
+        private String cardPassword;
 
     }
 
     @Getter
     public static class SponsorSignup{
-        String name;
-        String loginId;
-        String password;
-        String eqPassword;
-        String sponsorName;
-        String sponsorNumber;
-        Long categoryId;
+        @NotBlank(message = "이름은 필수입니다.")
+        private String name;
+
+        @NotBlank(message = "아이디는 필수입니다.")
+        private String loginId;
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
+
+        @NotBlank(message = "확인 비밀번호는 필수입니다.")
+        private String eqPassword;
+
+        @NotBlank(message = "협찬사 이름은 필수입니다.")
+        private String sponsorName;
+
+        @NotBlank(message = "사업자 번호는 필수입니다.")
+        private String sponsorNumber;
+
+        @NotNull(message = "카테고리는 필수입니다.")
+        private Long categoryId;
 
     }
 
     @Getter
     public static class Login{
 
-        String loginId;
-        String password;
+        @NotBlank(message = "아이디는 필수입니다.")
+        private String loginId;
+
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
 
     }
 
     @Getter
     public static class VerifyPassword{
 
-        String password;
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
 
     }
 
     @Getter
     public static class UpdateMember{
 
-        String password;
-        String newPassword;
-        String eqNewPassword;
+        @NotBlank(message = "비밀번호는 필수입니다.")
+        private String password;
+
+        @NotBlank(message = "새로운 비밀번호는 필수입니다.")
+        private String newPassword;
+
+        @NotBlank(message = "새로운 확인 비밀번호는 필수입니다.")
+        private String eqNewPassword;
 
     }
 
