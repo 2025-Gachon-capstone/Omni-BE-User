826b4da8a964dcd58d7edfef0c12180e77f8f3a0
Habeomsu <hbs5186@naver.com>
2025-04-18 20:26:47 +0900

    #37 스웨거 토큰 여부 표시

--- src/main/java/org/example/omnibeuser/controller/MemberController.java
+++ src/main/java/org/example/omnibeuser/controller/MemberController.java
@@ -27,7 +27,7 @@ public class MemberController {
     }
 
     @PostMapping("/signup/normal")
-    @Operation(summary = "일반 사용자 회원가입 API",description = "일반 사용자 회원가입을 위한 Api입니다.",tags = "Member")
+    @Operation(summary = "일반 사용자 회원가입 API",description = "일반 사용자 회원가입을 위한 Api입니다. ( 인증 필요없음 )",tags = "Member")
     public ApiResult<MemberResDto.NormalSignup> createNormalUser(@Valid @RequestBody MemberReqDto.NormalSignup normalSignupReqDto) {
 
         Member savedMember = memberService.createNormalMember(normalSignupReqDto);
@@ -36,7 +36,7 @@ public class MemberController {
     }
 
     @PostMapping("/signup/sponsor")
-    @Operation(summary = "협찬사 사용자 회원가입 API",description = "협찬사 사용자 회원가입을 위한 Api입니다.",tags = "Member")
+    @Operation(summary = "협찬사 사용자 회원가입 API",description = "협찬사 사용자 회원가입을 위한 Api입니다. ( 인증 필요없음 )",tags = "Member")
     public ApiResult<MemberResDto.SponsorSignup> createSponsorUser(@Valid @RequestBody MemberReqDto.SponsorSignup sponsorSignupDto) {
 
         Member savedMember = memberService.createSponsorMember(sponsorSignupDto);
@@ -46,7 +46,7 @@ public class MemberController {
 
     @PostMapping("/login")
     @Operation(summary = "로그인 API",
-            description = "로그인시 헤더에 발급되는 Authorization을 따로 보관해야 합니다.",
+            description = "로그인시 헤더에 발급되는 Authorization을 따로 보관해야 합니다. ( 인증 필요없음 )",
             tags = "Member")
     public ApiResult<?> login(@Valid @RequestBody MemberReqDto.Login loginReqDto) {
 
@@ -56,7 +56,7 @@ public class MemberController {
 
     @PostMapping("/logout")
     @Operation(summary = "로그아웃 API",
-            description = "로그아웃시 따로 보관한 Authorization을 삭제해주셔야 합니다.",
+            description = "로그아웃시 따로 보관한 Authorization을 삭제해주셔야 합니다. ( 쿠키 필요 )",
             tags = "Member")
     public ApiResult<?> logout() {
 
@@ -65,7 +65,7 @@ public class MemberController {
 
     @PostMapping("/password/verify")
     @Operation(summary = "비밀번호 인증 API",
-            description = "비밀번호 인증 Api 입니다.",
+            description = "비밀번호 인증 Api 입니다. ( 엑세스 토큰 필요 )",
             tags = "Member")
     public ApiResult<?> verify(@Parameter(hidden = true) @RequestHeader("X-Authorization-Id") String loginId,
                                @Valid @RequestBody MemberReqDto.VerifyPassword passwordDto) {
@@ -76,7 +76,7 @@ public class MemberController {
 
     @PutMapping("/members")
     @Operation(summary = "정보 수정 API",
-            description = "정보 수정 Api 입니다.",
+            description = "정보 수정 Api 입니다. ( 엑세스 토큰 필요 )",
             tags = "Member")
     public ApiResult<MemberResDto.UpdateMember> update(@Parameter(hidden = true) @RequestHeader("X-Authorization-Id") String loginId,
                                                        @Valid @RequestBody MemberReqDto.UpdateMember updateMemberDto){
@@ -88,7 +88,7 @@ public class MemberController {
 
     @DeleteMapping("/resign")
     @Operation(summary = "회원 탈퇴 API",
-            description = "탈퇴시 따로 보관한 Authorization을 삭제해주셔야 합니다.",
+            description = "탈퇴시 따로 보관한 Authorization을 삭제해주셔야 합니다. ( 엑세스 토큰 필요 )",
             tags = "Member")
     public ApiResult<MemberResDto.DeleteMember> resign(@Parameter(hidden = true) @RequestHeader("X-Authorization-Id") String loginId,
                                HttpServletResponse response){
@@ -103,7 +103,7 @@ public class MemberController {
     @GetMapping("/members/{loginId}")
     @Operation(summary = "멤버 아이디 가져오기 API",
             description = "서비스 통신 시 사용됩니다.",
-            tags = "Service")
+            tags = "Service-Member")
     public ApiResult<MemberResDto.GetMemberId> getMemberId(@PathVariable String loginId){
 
         return ApiResult.onSuccess(new MemberResDto.GetMemberId(memberService.findMemberIdByLoginId(loginId)));
