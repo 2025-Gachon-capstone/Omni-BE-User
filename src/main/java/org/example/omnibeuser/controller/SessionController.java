3990deb3028741f0ee014028d46dafd634a6c9f7
Habeomsu <hbs5186@naver.com>
2025-04-24 12:54:11 +0900

    #41 스웨거 응답값 수정

--- src/main/java/org/example/omnibeuser/controller/SessionController.java
+++ src/main/java/org/example/omnibeuser/controller/SessionController.java
@@ -1,6 +1,10 @@
 package org.example.omnibeuser.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -26,6 +30,13 @@ public class SessionController {
 
     @PostMapping("/reissue")
     @Operation(summary = "토큰 재발급 API",description = "로그인 후 사용이 가능합니다. ( 쿠키 필요 )",tags = "Session")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "COMMON200-성공",content = @Content(schema = @Schema(implementation = ApiResult.class))),
+            @ApiResponse(responseCode = "4001", description = "TOKEN4001-리프레쉬 토큰이 없습니다.",content = @Content(schema = @Schema(implementation = ApiResult.class))),
+            @ApiResponse(responseCode = "4002", description = "TOKEN4002-만료된 리프레쉬 토큰입니다.",content = @Content(schema = @Schema(implementation = ApiResult.class))),
+            @ApiResponse(responseCode = "4003", description = "TOKEN4003-유효하지 않은 리프레쉬 토큰입니다.",content = @Content(schema = @Schema(implementation = ApiResult.class))),
+            @ApiResponse(responseCode = "4004", description = "TOKEN4004-리프레쉬 토큰이 헤더에 없습니다.",content = @Content(schema = @Schema(implementation = ApiResult.class))),
+    })
     public ApiResult<?> reissue(HttpServletRequest request, HttpServletResponse response) {
         String refresh = null;
         Cookie[] cookies = request.getCookies();
