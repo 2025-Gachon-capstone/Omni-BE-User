57a6ffa2fdbc2b5162706eb31bf2ac069b691636
Habeomsu <hbs5186@naver.com>
2025-05-08 15:48:14 +0900

    #55 로그인 아이디를 통해 멤버 리스트 반환하기

--- src/main/java/org/example/omnibeuser/repository/MemberRepository.java
+++ src/main/java/org/example/omnibeuser/repository/MemberRepository.java
@@ -3,11 +3,13 @@ package org.example.omnibeuser.repository;
 import org.example.omnibeuser.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByMemberId(Long memberId);
     Optional<Member> findByLoginId(String loginId);
+    List<Member> findTop20ByLoginIdContaining(String loginId);
 
 }
