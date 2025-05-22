b77884ebe8efbb0ef7450b834a3715682b94a924
steamedEggMaster <iron.ch5014@gmail.com>
2025-05-22 23:53:57 +0900

    feat: EntityScan 추가 Test

--- src/main/java/org/example/omnibeuser/OmniBeUserApplication.java
+++ src/main/java/org/example/omnibeuser/OmniBeUserApplication.java
@@ -2,11 +2,13 @@ package org.example.omnibeuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "org.example.omnibeuser.client")
+@EntityScan(basePackages = "org.example.omnibeuser.entity")
 public class OmniBeUserApplication {
 
     public static void main(String[] args) {
