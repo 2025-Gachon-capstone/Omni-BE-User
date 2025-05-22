ad546fd7757a396acb6541c8b7e60a66c6255efd
steamedEggMaster <iron.ch5014@gmail.com>
2025-05-23 00:30:40 +0900

    feat: EnableJpaRepositories 추가 Test

--- src/main/java/org/example/omnibeuser/OmniBeUserApplication.java
+++ src/main/java/org/example/omnibeuser/OmniBeUserApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "org.example.omnibeuser.client")
 @EntityScan(basePackages = "org.example.omnibeuser.entity")
+@EnableJpaRepositories(basePackages = "org.example.omnibeuser.repository")
 public class OmniBeUserApplication {
 
     public static void main(String[] args) {
